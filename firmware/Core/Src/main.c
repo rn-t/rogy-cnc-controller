71e16f560681ad96eee46c9e707aca022cf89e24
Ken Yoshizoe <kenyoshizoe@gmail.com>
2021-10-17 07:48:56 +0900

    Change TIM1 Counter Period 72->60-1

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -265,7 +265,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 72;
+  htim1.Init.Period = 60-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
